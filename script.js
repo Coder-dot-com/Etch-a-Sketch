3ce41e5a51a11b279024321cb7bc1ced9d026487
coder-dot-com <usman17.s@hotmail.com>
2021-08-23 11:35:47 +0100

    Created vaadjustable grid size and clear button

--- script.js
+++ script.js
@@ -1,30 +1,79 @@
 gridContainer = document.querySelector("#gridContainer");
 
-let gridSize = 20
+let gridSizeSelector = document.querySelector("#gridSizeSelector");
 
-let r = document.querySelector(":root");
 
-function setSquareNumber(number) {
-    r.style.setProperty("--squareNumber", number);
+
+gridSizeSelector.oninput = function() {
+     gridSize = parseFloat(gridSizeSelector.value);
+     createGrid (gridSize);
 }
 
 
+let colorToAdd = "black"
+let root = document.querySelector(":root");
 
-let totalnumberOfSquares = gridSize * gridSize
 
-for (let i = 0; i < totalnumberOfSquares; i++) {
+
+
+
+function setSquareNumber(number) {
+    root.style.setProperty("--squareNumber", number);
+}
+
+function removeSquares () {
+    let allSquares = document.querySelectorAll(".gridSquareStyle")
+    allSquares.forEach(square => square.remove());
+}
+
+
+function createGrid (gridSize) {
+    let gridTotalArea = gridSize * gridSize;
+    removeSquares();
+    setSquareNumber(gridSize);
+    for (let i = 0; i < gridTotalArea; i++) {
     gridSquare = document.createElement('div');
     gridSquare.className = "gridSquareStyle";
-    setSquareNumber(gridSize);
     gridSquare.addEventListener('mouseover', addColorInClass);
     gridContainer.append(gridSquare);
 
 }
+}
 
+createGrid(16);
 
-function addColorInClass(e) {
-    e.target.style.backgroundColor = "black";
+// random color generator
+
+const hexValuesList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+function randomHexGen () {
+    randomHex = "#"
+    for (let i = 0; i < 6; i++) {
+        randomHex += hexValuesList[(Math.floor((Math.random())*16))]
+    }
+    return randomHex
 }
 
 
+
+
+function addColorInClass(e) {
+    e.target.style.backgroundColor = colorToAdd ;
+}
+
+
+// Reset Button
+
+
+resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener('click', colorReset)
+
+
+function colorReset() {
+    let allSquares = document.querySelectorAll(".gridSquareStyle")
+    allSquares.forEach(square => square.style.backgroundColor = "#ffffff");
+
+}
+
+// Grid Size Selector
 
